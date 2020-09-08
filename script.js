d699c63089430c3e5bd3c69246d00a8a3ab264c1
John Alonso <jvalon9455@gmail.com>
2020-09-07 20:20:41 -0400

    started naming variables in JS along with some pseudocode. Cleaned up HTML

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+// console.log("Hello World")
+
+var timeClock = document.getElementById("time-clock");
+var startButton = document.getElementById("start-btn");
+
+startButton.addEventListener('click', startQuiz)
+
+function startQuiz(){
+    // console.log("Started quiz")-IT WORKS!
+
+}
+
+function nextQuestion(){
+
+}
+
+function pickedAnswer(){
+
+}
