aaf032c60cd66f06e101cca6f793ddb5a2a543a3
John Alonso <jvalon9455@gmail.com>
2020-09-09 16:20:44 -0400

    got timer to work when start button is clicked. change text color to show timer.

--- script.js
+++ script.js
@@ -1,43 +1,67 @@
 // console.log("Hello World")
-var startCodingQuizBtn = document.getElementById("start-btn")
-var questionContainer = document.getElementById("question-container")
-var paragraphPrompt = document.getElementById("paragraph-prompt")
-var questionList = document.getElementById("question-list")
-var questionsAsked = document.getElementById("questions-asked")
-var answerBtn = document.getElementById("answer-btn")
+var startCodingQuizBtn = document.getElementById("start-btn");
+var questionContainer = document.getElementById("question-container");
+var paragraphPrompt = document.getElementById("paragraph-prompt");
+var questionEl = document.getElementById("question");
+var answerButtonsEl = document.getElementById("answers");
+var timeEl = document.querySelector(".time");
+var secondsLeft = 90;
 
-startCodingQuizBtn.addEventListener("click", startQuiz)
+startCodingQuizBtn.addEventListener("click", startQuiz);
 
-function startQuiz(){
+var quizQuestions, currentQuestionIndex
+
+// start the quiz and show the first question
+function startQuiz() {
     // console.log("Started quiz")It works!
-    startCodingQuizBtn.classList.add("hide")
-    paragraphPrompt.classList.add("hide")
-    questionContainer.classList.remove("hide")
-    nextQuestionEl()
+    setTime();
+    startCodingQuizBtn.classList.add("hide");
+    paragraphPrompt.classList.add("hide");
+    quizQuestions = questionPrompts.sort(() => Math.random());
+    currentQuestionIndex = 0
+    questionContainer.classList.remove("hide");
+    // nextQuestionEl();
 
-}
+};
 
-function nextQuestionEl(){
+// function nextQuestionEl() {
+//     showQuizQuestion(quizQuestions[currentQuestionIndex])
+// }
 
-}
+// // shows next question and answer choices
+// // function showQuizQuestion(question) {
+// //     questionEl.innerText = question.question;
+// //     question.answers.forEach(answer => {
+// //         var button = document.createElement("button");
+// //         button.innerText = answer.text;
+// //         button.classList.add("btn");
+// //         button.addEventListener("click", selectAnswer);
+// //         answerButtonsEl.
+// //     });
+// // }
+// function selectAnswer(e) {
 
-function showNextQuestion(question){
-    questionsAsked.innerText = question.question 
+// }
 
-}
+function setTime() {
+    var timerInterval = setInterval(function(){
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
 
-function selectAnswer(){
-
+        if(secondsLeft === 0){
+            clearInterval(timerInterval);
+        }
+    }, 1000);
 }
 
 var questionPrompts = [
     {
-        question: "Commonly used data types DO NOT include",
+        question: "Commonly used data types DO NOT include:",
         answers: [
-            {text: "strings", correct: false},
-            {text: "booleans", correct: false},
-            {text: "alerts", correct: true},
-            {text: "numbers", correct: false},
+            { text: "strings", correct: false },
+            { text: "booleans", correct: false },
+            { text: "alerts", correct: true },
+            { text: "numbers", correct: false },
         ]
     }
 ]
