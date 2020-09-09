53c458ab13eee719553a6eee51224d306655d4d9
John Alonso <jvalon9455@gmail.com>
2020-09-08 21:38:26 -0400

    added more variables to call upon in JS. also added divs in html to call upon

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 var startCodingQuizBtn = document.getElementById("start-btn")
 var questionContainer = document.getElementById("question-container")
 var paragraphPrompt = document.getElementById("paragraph-prompt")
+var questionList = document.getElementById("question-list")
+var questionsAsked = document.getElementById("questions-asked")
+var answerBtn = document.getElementById("answer-btn")
 
 startCodingQuizBtn.addEventListener("click", startQuiz)
 
@@ -10,6 +13,7 @@ function startQuiz(){
     startCodingQuizBtn.classList.add("hide")
     paragraphPrompt.classList.add("hide")
     questionContainer.classList.remove("hide")
+    nextQuestionEl()
 
 }
 
@@ -17,7 +21,23 @@ function nextQuestionEl(){
 
 }
 
+function showNextQuestion(question){
+    questionsAsked.innerText = question.question 
+
+}
 
 function selectAnswer(){
 
 }
+
+var questionPrompts = [
+    {
+        question: "Commonly used data types DO NOT include",
+        answers: [
+            {text: "strings", correct: false},
+            {text: "booleans", correct: false},
+            {text: "alerts", correct: true},
+            {text: "numbers", correct: false},
+        ]
+    }
+]
