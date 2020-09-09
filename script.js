d59e281a09a5aa95b95c571a8de0dae0d6547469
John Alonso <jvalon9455@gmail.com>
2020-09-08 20:59:21 -0400

    added buttons to html. updated CSS. started JS from scratch again..

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 
 var timeClock = document.getElementById("time-clock");
 var startButton = document.getElementById("start-btn");
-var questionConEl = document.getElementById("question-container")
+var buttonList = document.getElementById("answer-btn");
 
 startButton.addEventListener("click", startQuiz)
 
 function startQuiz(){
     console.log("Started quiz")
     startButton.classList.add("hide")
-    questionConEl.classList.remove("hide")
+    buttonList.classList.remove("hide")
+    nextQuestion()
 
 }
 
@@ -31,43 +32,4 @@ var quizQuestionArray = [
         { text: "numbers", correct: false },
       ],
     },
-    {
-      question: "The condition in an if/else statement is enclosed within:",
-      answers: [
-        { text: "quotes", correct: false },
-        { text: "curly brackets", correct: false },
-        { text: "parentheses", correct: true },
-        { text: "square brackets", correct: false },
-      ],
-    },
-    {
-      question: "Arrays in JavaScript can be used to store:",
-      answers: [
-        { text: "numbers and strings", correct: false },
-        { text: "other arrays", correct: false },
-        { text: "booleans", correct: false },
-        { text: "all of the above", correct: true },
-      ],
-    },
-    {
-      question:
-        "String values must be enclosed within ______ when being assigned to variables:",
-      answers: [
-        { text: "commas", correct: false },
-        { text: "curly brackets", correct: false },
-        { text: "quotes", correct: true },
-        { text: "parentheses", correct: false },
-      ],
-    },
-    {
-      question:
-        "A very useful tool used during development and debugging for printing content to the debugger is:",
-      answers: [
-        { text: "JavaScript", correct: false },
-        { text: "terminal / bash", correct: false },
-        { text: "for loops", correct: false },
-        { text: "console.log", correct: true },
-      ],
-    },
-  ];
-  
+]
