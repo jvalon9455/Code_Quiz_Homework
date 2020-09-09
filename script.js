f6b89c43acb252e4501cf5b21a7310c7bac4b6ec
John Alonso <jvalon9455@gmail.com>
2020-09-09 19:45:19 -0400

    added ids to buttons to make it easier to call upon

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var answerButtonsEl = document.getElementById("answers");
 var timeEl = document.querySelector(".time");
 var secondsLeft = 90;
 
+
 startCodingQuizBtn.addEventListener("click", startQuiz);
 
 var quizQuestions, currentQuestionIndex
@@ -18,50 +19,48 @@ function startQuiz() {
     startCodingQuizBtn.classList.add("hide");
     paragraphPrompt.classList.add("hide");
     quizQuestions = questionPrompts.sort(() => Math.random());
-    currentQuestionIndex = 0
+    currentQuestionIndex = 0;
     questionContainer.classList.remove("hide");
-    // nextQuestionEl();
+    nextQuestionEl();
 
 };
-
-// function nextQuestionEl() {
-//     showQuizQuestion(quizQuestions[currentQuestionIndex])
-// }
-
-// // shows next question and answer choices
-// // function showQuizQuestion(question) {
-// //     questionEl.innerText = question.question;
-// //     question.answers.forEach(answer => {
-// //         var button = document.createElement("button");
-// //         button.innerText = answer.text;
-// //         button.classList.add("btn");
-// //         button.addEventListener("click", selectAnswer);
-// //         answerButtonsEl.
-// //     });
-// // }
-// function selectAnswer(e) {
-
-// }
-
-function setTime() {
-    var timerInterval = setInterval(function(){
-        secondsLeft--;
-        timeEl.textContent = secondsLeft;
-
-        if(secondsLeft === 0){
-            clearInterval(timerInterval);
-        }
-    }, 1000);
-}
-
-var questionPrompts = [
-    {
-        question: "Commonly used data types DO NOT include:",
-        answers: [
-            { text: "strings", correct: false },
-            { text: "booleans", correct: false },
-            { text: "alerts", correct: true },
-            { text: "numbers", correct: false },
-        ]
+ function showQuizQuestion (question) {
+    questionEl.innerText = question.question;
+    question.answer.forEach(answer => {
+        var button = document.createElement("button");
+        button.innerText = answer.text;
+        button.classList.add("btn");
+        button.addEventListener("click", selectAnswer);
+        answerButtonsEl.appendChild(button);
+    });
+ }
+    function nextQuestionEl() {
+        showQuizQuestion(quizQuestions[currentQuestionIndex])
     }
-]
+
+    function selectAnswer(e) {
+
+    }
+
+    function setTime() {
+        var timerInterval = setInterval(function () {
+            secondsLeft--;
+            timeEl.textContent = secondsLeft;
+
+            if (secondsLeft === 0) {
+                clearInterval(timerInterval);
+            }
+        }, 1000);
+    }
+
+    var questionPrompts = [
+        {
+            question: "Commonly used data types DO NOT include:",
+            answers: [
+                { text: "strings", correct: false },
+                { text: "booleans", correct: false },
+                { text: "alerts", correct: true },
+                { text: "numbers", correct: false },
+            ]
+        }
+    ]
